eb1ab4b883e3a985388e23658d920895221ff6b0
Duarte Silva <desenvolvimento@serializing.me>
2018-05-30 18:46:53 +0200

    Major updates to the theme:
    - Updated Bootstrap, FontAwesome, jQuery
    - Removed unnecessary CSS
    - Removed Glyphicons
    - Moved shortcodes from content to the theme
    - Updated all layouts based on the changes above

--- static/js/serializingme.js
+++ static/js/serializingme.js
@@ -1,6 +1,6 @@
 $(function() {
   $('a:not([href^="http://'+location.hostname+'"],[href^="https://'+location.hostname+'"],[href^="/"],[href^="mailto:"],[class*="footer-link"])').each(function() {
-    $(this).append('<span>&nbsp;<i class="fa fa-external-link"></i></span>');
+    $(this).append('<span>&nbsp;<i class="fas fa-external-link-alt"></i></span>');
   });
   $('a[href^="mailto:"]').each(function() {
     this.href = this.href.replace(/arroba/g, '@').replace(/ponto/g, '.');
